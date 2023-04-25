c14a19a86062824fd26057e9dc5ec1ffd056a88e
sunsyc <64606509+sunsyc@users.noreply.github.com>
2023-04-24 21:36:09 -0400

    Added GamePaused screen
    applied Time.timeScale = 0f; which to let game "stop" when the gamepaused screen or game over screen shows

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject gameOverUI;
+    public GameObject gamePausedUI;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameOverUI.activeInHierarchy)
+        if (gameOverUI.activeInHierarchy || gamePausedUI.activeInHierarchy)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
@@ -26,11 +28,31 @@ public class GameManagerScript : MonoBehaviour
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                resume();
+            }
+            else
+            {
+                gamePaused();
+            }
+        }
     }
 
     public void gameOver()
     {
         gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void gamePaused()
+    {
+        gamePausedUI.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
     }
 
     public void restart()
@@ -42,5 +64,12 @@ public class GameManagerScript : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+
+    public void resume()
+    {
+        gamePausedUI.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
     }
 }
